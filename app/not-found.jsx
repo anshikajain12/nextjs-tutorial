da27ec9c1c9d59fc7f420a115a091463cdbc857a
anshika <nisthajain2412@gmail.com>
2024-07-03 06:09:49 +0530

    not-found page created

---
+++ app/not-found.jsx
@@ -0,0 +1,8 @@
+export default function NotFound(){
+    return(
+        <div>
+            <h1> Page not found</h1>
+            <h4>Could not find requested resources</h4>
+        </div>
+    )
+}
--- app/product/[productId]/reviews/[reviewId]/page.tsx
+++ app/product/[productId]/reviews/[reviewId]/page.tsx
@@ -1,9 +1,13 @@
+import { notFound } from "next/navigation"
 export default function ReviewDetails({params}:{
     params:{
         productId:string,
         reviewId:string
     }
 }) {
+    if(parseInt(params.reviewId)>1000){
+        notFound();
+    }
     return (
         <>
             Review: {params.reviewId} for product: {params.productId}
