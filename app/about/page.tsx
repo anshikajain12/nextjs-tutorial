def09eba08151c0ec1cc524f9c0ba15541181f59
anshika <nisthajain2412@gmail.com>
2024-07-06 08:19:07 +0530

    Metadata concept

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,3 +1,7 @@
+export const metadata={
+    title:"About MetaData"
+}
+
 export default function About() {
     return (
         <>
--- app/product/[productId]/page.tsx
+++ app/product/[productId]/page.tsx
@@ -1,6 +1,19 @@
-export default function DynamicProductDetails({params}:{
+import { Metadata } from "next";
+
+export const generateMetadata= async ({params}:props):Promise<Metadata>=>{
+    const title= await new Promise(resolve=>{
+        setTimeout(() => {
+            resolve(`iPhone ${params.productId}`)
+        }, 100);
+    })
+    return {
+        title: `Product ${title}`,
+    }
+}
+type props={
     params:{productId:string};
-}) {
+}
+export default function DynamicProductDetails({params}:props) {
     return (
         <>
             Details about product {params.productId}
