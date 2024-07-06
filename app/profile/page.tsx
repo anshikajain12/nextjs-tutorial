b9a49853bba2324f9348653120164e3550f5f08f
anshika <nisthajain2412@gmail.com>
2024-07-06 08:29:58 +0530

    title metadata format

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,3 +1,10 @@
+import { Metadata } from "next"
+
+export const metadata:Metadata={
+    title:{
+        absolute:"Profile", //when you use absolute in metadata it properly ignored the template which was added in the app/layout.tsx file
+    }
+}
 export default function Profile() {
     return (
         <>
