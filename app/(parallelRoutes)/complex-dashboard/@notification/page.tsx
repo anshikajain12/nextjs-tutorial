d465fa6c659feff8a5e662af48a61cc53b10d897
anshika <nisthajain2412@gmail.com>
2024-07-11 07:12:49 +0530

    understanding of parallel routes, unmatched routes and conditional routes

---
+++ app/(parallelRoutes)/complex-dashboard/@notification/page.tsx
@@ -0,0 +1,9 @@
+import Card from "@/components/Card";
+import Link from "next/link";
+
+export default function Notification(){
+    return <Card>
+        <div>Notification!!</div>
+        <Link href="../complex-dashboard/archived">Archived</Link>
+        </Card>
+}
